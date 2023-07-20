b8ddf9e10f53e322197022558091a1e1761a6186
aryansingh-123 <aksingh2558@gmail.com>
2023-07-21 02:13:42 +0530

    final commit

---
+++ src/components/ListCard.js
@@ -0,0 +1,14 @@
+import React from "react";
+import "./listcard.css";
+function ListCard() {
+  return (
+    <div className="list_card">
+      <div className="info">
+        <h3>Python with Django</h3>
+        <p>Online , at Night ০9 : ০০ PM</p>
+      </div>
+    </div>
+  );
+}
+
+export default ListCard;
